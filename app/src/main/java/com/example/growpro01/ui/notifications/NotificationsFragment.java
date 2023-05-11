1a69cb208758ac7874da7d918be96880805f56d8
Maitena Rosa <maitenamariaros.alumn@cescristorey.com>
2023-05-11 22:09:53 +0200

    layouts actualizadas

---
+++ app/src/main/java/com/example/growpro01/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,30 @@
+package com.example.growpro01.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.growpro01.R;
+import com.example.growpro01.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+    public View onCreateView( LayoutInflater inflater,
+                              ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+}
